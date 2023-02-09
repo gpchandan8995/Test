d8b94cac48acda9df00303c8b434ff3bfd34a969
D6_63232_gaurav <gpchandan8995@gmail.com>
2023-02-09 18:50:31 +0530

    by connecting the sql database Item and Iuser table created

---
+++ dbo.Iuser.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[Iuser] (
+    [Uid]    INT           IDENTITY (1, 1) NOT NULL,
+    [Uemail] NVARCHAR (50) NOT NULL,
+    [Upass]  VARCHAR (50)  NOT NULL,
+    [Urole]  SMALLINT      NOT NULL,
+    PRIMARY KEY CLUSTERED ([Uid] ASC)
+);
+
---
+++ dbo.Table.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE Item
+(
+	[Id] INT NOT NULL PRIMARY KEY identity, 
+    [Iname] NVARCHAR(50) NOT NULL, 
+    [Idescription] NVARCHAR(50) null, 
+    [Iprice] INT NOT NULL, 
+    [IaddDate] DATETIME NOT NULL, 
+    [IexpDate] DATE null , 
+    [Iavailable] NCHAR(10) NOT NULL
+)
